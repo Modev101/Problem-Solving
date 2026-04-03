3ab2e36b1ef347736b65de215383d8fa65d40a02
Modev101 <bettachemohamed333@gmail.com>
2026-04-03 01:14:30 +0100

    Add files via upload

---
+++ cpp/7kyu/AreTheNumbersInOrder.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+bool isAscOrder(const std::vector<int>& arr) {
+    for (size_t i = 1; i < arr.size(); i++) {
+        if (arr[i - 1] > arr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
