638290f14ec2d9981730abcfc1bc22693e3b993f
Modev101 <bettachemohamed333@gmail.com>
2026-04-04 05:25:15 +0100

    Add files via upload

---
+++ cpp/8kyu/Abbreviate a Two Word Name.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+std::string abbrevName(std::string name)
+{
+ std:: string result = "" ;
+  result += toupper(name[0]) ;
+  result += "." ;
+  result += toupper(name[name.find(" ") + 1]) ;
+  return result ;
+}
