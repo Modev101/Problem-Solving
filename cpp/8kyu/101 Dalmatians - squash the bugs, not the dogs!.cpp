0b98c8c4be82e5850dbf014548fe5fdba64ca319
Modev101 <bettachemohamed333@gmail.com>
2026-04-04 05:24:59 +0100

    Add files via upload

---
+++ cpp/8kyu/101 Dalmatians - squash the bugs, not the dogs!.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <string>
+
+std::string howManyDalmatians(int number )
+   { 
+  std::vector <std::string> dogs = {"Hardly any",
+                                    "More than a handful!",
+                                    "Woah that's a lot of dogs!",
+                                    "101 DALMATIONS!!!" } ;
+
+return (number <= 10) ? dogs[0] :
+       (number <= 50) ? dogs[1] :
+       (number <= 100 )? dogs[2] :
+        dogs[3];
+    }
