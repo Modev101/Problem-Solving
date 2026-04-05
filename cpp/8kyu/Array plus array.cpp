f5f53c0ba54f6029ab7b751c6ad9f292cf6d53e2
Modev101 <bettachemohamed333@gmail.com>
2026-04-05 06:36:11 +0100

    Add files via upload

---
+++ cpp/8kyu/Array plus array.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+int arrayPlusArray(std::vector<int> a, std::vector<int> b) {
+ int sum = 0 ;
+ for( int num : a)  sum += num ;
+ for( int num : b)  sum += num ;
+  return sum ; 
+}
