b98654af79181df23411d0a3c4c3ee32cd47c4bd
Modev101 <bettachemohamed333@gmail.com>
2026-04-16 18:45:50 +0100

    Add files via upload

---
+++ cpp/8kyu/Count by X.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+
+vector<int> countBy(int x, int n) {
+    vector<int> result;
+    for (int i = 1; i <= n; i++) {
+        result.push_back(x * i);
+    }
+    return result;
+}
