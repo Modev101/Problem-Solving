17d2e2cee8812311e2c0007e2c91512e41892c7c
Modev101 <bettachemohamed333@gmail.com>
2026-04-17 10:50:57 +0100

    Add files via upload

---
+++ cpp/8kyu/Counting sheep.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std; 
+
+int count_sheep(vector<bool> arr) 
+{
+  int result = 0 ;
+  for ( int i = 0 ; i < arr.size() ; i++)
+    {
+    if (arr[i])
+      {
+      result++ ;
+      }
+    
+    }
+  return result ;
+}
