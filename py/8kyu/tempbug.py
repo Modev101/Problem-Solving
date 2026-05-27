4db5f48ef859cff71be731705b2cada8cd03d5e7
Modev101 <bettachemohamed333@gmail.com>
2026-05-27 16:04:04 +0100

    Add files via upload

---
+++ py/8kyu/tempbug.py
@@ -0,0 +1,11 @@
+def weather_info(temp):
+    c = convert_to_celsius(temp)
+    
+    if c <= 0:
+        return f"{c} is freezing temperature"
+    else:
+        return f"{c} is above freezing temperature"
+
+
+def convert_to_celsius(temperature):
+    return (temperature - 32) * (5/9)
