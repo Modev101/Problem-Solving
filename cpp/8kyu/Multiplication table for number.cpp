661dcd6fafa58a3e9d83716a5d4d4f84a52b6431
Modev101 <bettachemohamed333@gmail.com>
2026-06-12 17:00:16 +0100

    Add files via upload

---
+++ cpp/8kyu/Multiplication table for number.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+std::string multi_table(int number) {
+    std::string result = "";
+    for (int i = 1; i <= 10; i++) {
+        result += std::to_string(i) + " * " + std::to_string(number) + " = " + std::to_string(i * number);
+        if (i != 10) {
+            result += "\n"; 
+        }
+    }
+    return result;
+}
