e58d009e58f1453337379e628815b0fcae08a278
Modev101 <bettachemohamed333@gmail.com>
2026-06-21 19:53:40 +0100

    Add files via upload

---
+++ cpp/8kyu/Powers of 2.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <cstdint>
+#include <cmath>
+
+std::vector<uint64_t> powers_of_two(int n) {
+std::vector<uint64_t> result ;
+  for (int i = 0 ; i <= n ; i++)
+    {
+    result.push_back(pow(2, i));
+    }
+  return result;
+}
